56db0908a64c0f6ccc6fa4bfaa67ab0d8d2c18a2
Bieder, Martin <bieder@kdo.de>
2018-07-18 09:17:28 +0200

    refactor: remove ruby, npm packages and branches

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,7 +2,8 @@
 
 set -xe
 
-docker build -t kdose/symfony-build:7.1 .
+docker build -t kdose/symfony-build:7.1 7.1
 docker push kdose/symfony-build:7.1
-docker build -t kdose/symfony-build:latest .
+docker build -t kdose/symfony-build:latest -t kdose/symfony-build:7.2 7.2
 docker push kdose/symfony-build:latest
+docker push kdose/symfony-build:7.2
