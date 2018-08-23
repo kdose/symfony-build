03020050456b5c984554f3ebb268dd4817552bf7
Bieder, Martin <bieder@kdo.de>
2018-08-23 13:52:17 +0200

    feat: add php 7.3

--- build-and-push.sh
+++ build-and-push.sh
@@ -7,3 +7,5 @@ docker push kdose/symfony-build:7.1
 docker build -t kdose/symfony-build:latest -t kdose/symfony-build:7.2 7.2
 docker push kdose/symfony-build:latest
 docker push kdose/symfony-build:7.2
+docker build -t kdose/symfony-build:7.3 7.3
+docker push kdose/symfony-build:7.3
